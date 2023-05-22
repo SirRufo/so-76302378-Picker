ef603cd41166bf1b5b5c876752051be7a1015a77
Oliver Münzberg <oliver.muenzberg@gmail.com>
2023-05-22 17:45:28 +0200

    CollectionView added

--- MyApp/ViewModels/BindingSourcePageViewModel.cs
+++ MyApp/ViewModels/BindingSourcePageViewModel.cs
@@ -3,4 +3,6 @@
 public class BindingSourcePageViewModel : ViewModelBase
 {
     [Reactive] public string Value { get; set; } = "Hello World";
+
+    public IList<int> Items { get; } = new List<int> { 1, }.AsReadOnly();
 }
